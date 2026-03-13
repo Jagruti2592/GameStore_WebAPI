2039937c992b458b934a54cb198f58297d52d5ad
Jagruti Chitte <81913740+Jagruti2592@users.noreply.github.com>
2026-03-13 13:39:18 -0400

    Add data seeding logic.

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,7 @@
 using GameStore.Api.Data;
 using GameStore.Api.Dtos;
 using GameStore.Api.EndPoints;
+using GameStore.Api.Models;
 using Microsoft.AspNetCore.Http.HttpResults;
 
 
@@ -10,7 +11,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddValidation();
 
 var connString = "Data Source = GameStore.db";
-builder.Services.AddSqlite<GameStoreContext>(connString);
+builder.Services.AddSqlite<GameStoreContext>(
+    connString,
+    optionsAction: options => options.UseSeeding((context, _) =>
+    {
+        if (!context.Set<Genre>().Any())
+        {
+            context.Set<Genre>().AddRange(
+                new Genre { Name = "Action" },
+                new Genre { Name = "Adventure" },
+                new Genre { Name = "RPG" },
+                new Genre { Name = "Strategy" },
+                new Genre { Name = "Sports" }
+            );
+            context.SaveChanges();
+        }
+    })
+    );
 
 var app = builder.Build();
 
--- GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2606c0d2159a7f4817e27dcf95551baa4cb58474")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
